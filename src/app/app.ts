5392b1ae13efecab27d9a0f4430dd8c81a47a6ed
shubhojitroy <royshubhojit@gmail.com>
2025-07-10 04:51:35 +1000

    initial push to repo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, ContactComponent, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'oadynamiccss-extraction';
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  title = 'OA Application';
+  constructor(private router: Router) {}
+
+  goToRoute(route: string) {
+    this.router.navigate([route]);
+  }
+}
